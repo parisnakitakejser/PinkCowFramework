dca650d0842335ca2e1f230d0ed706c5efb9a5b0
Paris Nakita Kejser <contact@parisnakitakejser.com>
2013-11-25 10:04:48 +0100

    (new) added Util\Html support class now

---
+++ Util/Html/build.php
@@ -0,0 +1,39 @@
+<?php
+namespace PinkCow\Util;
+
+class Html
+{
+	/**
+	 * @author Paris Nakita Kejser
+	 * @since 1.0.0.6
+	 * @version 1.0.0.6
+	 * @access public
+	 *
+	 * @param array $array
+	 * @param string $name
+	 * @param string $id
+	 * @param string $selected
+	 * @return string
+	 */
+	public static function buildSelect($array = array(), $name = '', $id = '', $selected = '')
+	{
+		$contect = '<select>';
+		
+		if ( count( $array ) > 0 )
+		{
+			foreach( $array AS $key => $val )
+			{
+				$contect .= '<option value="'. $key .'"'. ( $key == $selected ? ' selected' : '' ) .'>'. $val .'</option>';
+			}
+		}
+		else
+		{
+			$contect .= '<option>{missing:array}</option>';
+		}
+		
+		$contect .= '</select>';
+		
+		return $contect;
+	}
+}
+?>
