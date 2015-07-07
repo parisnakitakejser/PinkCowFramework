80086a5726fe7a0160f39c44d454ad8ed58f14ac
Paris Nakita Kejser <paris@fcomputer.dk>
2015-07-07 14:09:53 +0200

    (update) add comments

--- Util/Encode/build.php
+++ Util/Encode/build.php
@@ -3,10 +3,28 @@ namespace PinkCow\Util;
 
 class Encode
 {
+	/**
+	 * @author Paris Nakita Kejser
+	 * @since 1.1.0.0
+	 * @version 1.1.0.0
+	 * @access public
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
 	public static function win1252ToUtf8($string) {
 		return iconv('windows-1252', 'UTF-8', $string);
 	}
 	
+	/**
+	 * @author Paris Nakita Kejser
+	 * @since 1.1.0.0
+	 * @version 1.1.0.0
+	 * @access public
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
 	public static function ibm850ToUtf8($string) {
 		return iconv('IBM850', 'UTF-8', $string);
 	}
